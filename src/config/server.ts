dbbc167ca2afedc12eb3961dd1d8d1e936f24f31
francorinque <francoagustinrinquecorreo@gmail.com>
2024-07-20 12:04:03 -0300

    a

---
+++ src/config/server.ts
@@ -0,0 +1,13 @@
+// export class Server {
+//   public app: any;
+
+//   constructor() {}
+
+//   port: number = 3000;
+
+//   listen() {
+//     this.app.listen(this.port, () => {
+//       console.log(`Servidor corriendo en el puerto ${this.port}`);
+//     });
+//   }
+// }
